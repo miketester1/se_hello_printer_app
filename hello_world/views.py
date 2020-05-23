659f14308f4d32728d95c58dab9b15da2df06245
miketester1 <miketester1@users.noreply.github.com>
2020-05-23 07:30:37 +0000

    porawienie lintera i zmiana imienia

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Michal_M-dodatkowy text"
+moje_imie = "Michal_MM"
 msg = "Hello World!"
 
 
